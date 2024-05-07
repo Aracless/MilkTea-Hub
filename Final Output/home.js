5710ff3d9136f7405cd8949079ed957b6ae7d076
ARA <119785590+Aracless@users.noreply.github.com>
2024-05-07 16:57:12 +0800

    Add files via upload

---
+++ Final Output/home.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+
+        let menu = document.querySelector('#menu-icon');
+        let navlist = document.querySelector('.navlist');
+
+        menu.onclick = () => {
+
+            menu.classList.toggle('bx-x');
+            navlist.classList.toggle('open');
+        }
+
+    </script>
